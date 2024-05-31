1d171e76372b511ef2d01ffe6661c2b328f6a702
Hussein Chawich <h9109@hotmail.com>
2024-05-31 23:10:18 +0300

    init

--- todoList/models.py
+++ todoList/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class listItems(models.Model):
+    name = models.CharField(max_length=50)
+
+class task(models.Model):
+    listName = models.ForeignKey(listItems, on_delete=models.CASCADE)
+    dateCreated = models.DateField(initial = today, required = True)
+    taskText = models.CharField(max_length=max, required = True)
+    complete = models.BinaryField(default=0)
